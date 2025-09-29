4e148836b4ae4c24007d47ac592254e0254fa531
Faizan Ali <fizcq354@outlook.com>
2025-09-29 17:43:16 +0500

    setTimeOut.js , setTimeOut.html

--- setTimeOut.js
+++ setTimeOut.js
@@ -1,6 +1,7 @@
 // setTimeOut = function in JS that allows you to schedule.
 //              the excution of a after an amont of time  (milliseconds).
 //              Times are approptiate (varies based on the Workload of the JS runtime env.).
+// ClearTimeout(timeoutID) =  can cancel a timeout before it triggers.
 
 //             setTimeOut(call back , delay);
 
@@ -11,6 +12,25 @@
 
 //setTimeOut(setTimeOut , 3000);
 
-setTimeout(() =>  window.alert("Hello Guys") , 4000);
+//setTimeout(() =>  window.alert("Hello Guys") , 4000);
 
+
+//const timeoutID  = setTimeOut(() => window.alert(`Js is cool`), 4000);
+
+
+//ClearTimeout(timeoutID);
+
+
+let timeoutID;
+
+
+function startTimer(){
+     timeoutID  =  setTimeout(()  => window.alert(`Hello JS`), 5000);
+     console.log("Count is Started");
+}
+
+function clearTimer(){
+   clearTimeout(timeoutID);
+   console.log("Count is End");
+}
 
