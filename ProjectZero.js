2ab09807ff7b6145f952bae1a670cd8b069c0db0
Faizan Ali <fizcq354@outlook.com>
2025-11-02 19:10:56 +0500

    ProjectZero.js

--- ProjectZero.js
+++ ProjectZero.js
@@ -1,4 +1,6 @@
+// Game ROCK , PAPER AND SCISSORS AND ALL JS PROGRAMS AND ALL CODE AND LOGIC
 
+// We use array for better FUNCTION WORKING OF Math.random and also all computerChoice feature
 
 const Choices = ["rock" , "paper" , "scissors"];
 
@@ -6,11 +8,22 @@ const PlayerDisplay = document.getElementById("playerDisplay");
 const ComputerDisplay = document.getElementById("computerDisplay");
 const ResultsDisplay = document.getElementById("resultDisplay");
 
+
+// BUILT FUNCTION FOR ALL INTERACTIVITY BUTTON ON UI FOR GAME.
 function playGame(PlayerChoice) {
-       
-      const computerChoice = Choices[Math.floor(Math.random() *3)];
-      
+
+ // TESTING  THE [OPPONENT or PLAYER2 or COMPUTER CHOICE] =>  FEATURE = 
+ // const computerChoice = Choices[Math.floor(Math.random() * 3)];
+ // and Testing use console.log(computerChoice)  see this feature 
+ // works well and also behave well in longer code JS CODE.
+    
+        const computerChoice = Choices[Math.floor(Math.random() * 3)];
+
+ // First Results are at Zero Stage and No Result on UI LIVE ON WEBPAGE AND DEVTOOL 
+
      let results = "";
+// BUILDING ALL COMPARSION LOGIC FOR PLAYERCHOICE AND COMPUTERCHOICE FEATURE AND ON 
+// THERE INTERACTVITY RESPONSE ON UI
 
      if(PlayerChoice  === computerChoice){
        results = "MATCH IS TIE"
@@ -30,10 +43,10 @@ function playGame(PlayerChoice) {
             
         }
       
-     }
+     };
         
     PlayerDisplay.textContent = `PLAYER : ${PlayerChoice}`;
     ComputerDisplay.textContent = `COMPUTER : ${computerChoice}`;
     ResultsDisplay.textContent = `RESULTS : ${results}`;
 
-}
+};
