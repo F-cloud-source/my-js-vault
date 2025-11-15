1cdd0685940fc3f7ab43c54c940f13a920807339
Faizan Ali <fizcq354@outlook.com>
2025-11-15 15:39:41 +0500

    Async-Await.js

--- Async-Await.js
+++ Async-Await.js
@@ -1,72 +1,108 @@
 // ASYNC AND AWAIT = Async = Make a function that Return a promise
-//                   Await = make a async function that return a promise
+//                   Await = make a async function that return a promise 
 
 
 //  Allows you to Write your Asyncronous code in syncronous Manner 
 //  Async doesn't have resolve or reject parameters 
-//  Ecerything after Await is place in a Event quene
+//  Everything after Await is place in a Event quene
 
 
-function checkNumber(num) {
-  return new Promise((resolve, reject) => {
-    if (num > 10) {
-      resolve("Number is big enough");
-    } else {
-      reject("Number is too small");
-    }
+
+// Home Chores JS PROGRAM IN  PROMISE AND SETTIMEOUT FUNCTION AND DIFFERENT TASKS.
+
+  function walkDog(){
+  return new Promise((resolve , reject) => {
+    setTimeout(() => {
+       
+        const walkDog = true;
+        
+        if(walkDog){
+          resolve("We walk 🚶‍♀️‍➡️and Feed the dog🐶 and Walk back to Home🏠");
+        }
+        else{
+          reject("We do not walk the dog Today");
+        }
+        
+    }, 2000);
   });
 }
 
-checkNumber(15)
-  .then((msg) => {
-    console.log("Step 1:", msg);
-    return 20;
-  })
-  .then((value) => {
-    console.log("Step 2:", value);
-    if (value % 2 === 0) {
-      return "Even number, passing on";
-    } else {
-      throw new Error("Odd number error");
-    }
-  })
-  .then((msg) => {
-    console.log("Step 3:", msg);
-  })
-  .catch((err) => {
-    console.log("Caught error:", err);
-  });
-
-
-  function addFive(num) {
-  return new Promise((resolve, reject) => {
-    if (typeof num === "number") {
-      resolve(num + 5);
-    } else {
-      reject("Not a number!");
-    }
+function CleanHome(){
+  return new Promise((resolve , reject) => {
+    setTimeout(() => {
+       
+        const CleanHome = true;
+        
+        if(CleanHome){
+          resolve("We cleaned All home 🏠 and AlL items of home like Shelfes and Cupboards and kitchens and BedRoom and Toilets");
+        }
+        else{
+          reject("We did not Cleaned the home Todya because Maid not come");
+        }
+        
+    }, 2800);
   });
 }
 
-function multiplyByTwo(num) {
-  return new Promise((resolve) => {
-    resolve(num * 2);
+
+function CarWash(){
+  return new Promise((resolve , reject) => {
+    setTimeout(() => {
+       
+        const CarWash = true;
+        
+        if(CarWash){
+          resolve("We  Wash 🫧🧼 the Car🚗🚘");
+        }
+        else{
+          reject("We do not wash the Car today");
+        }
+        
+    }, 3500);
   });
 }
 
-// Chained Promises
-addFive(10)
-  .then((result) => {
-    console.log("After adding 5:", result);
-    return multiplyByTwo(result);
-  })
-  .then((result) => {
-    console.log("After multiplying by 2:", result);
-    return addFive("oops"); // intentionally wrong type
-  })
-  .then((result) => {
-    console.log("This won't run:", result);
-  })
-  .catch((err) => {
-    console.log("Caught an error:", err);
-  });
+
+async function DoChores(){
+   try{
+
+    const DogResults = await walkDog()
+    console.log(DogResults);
+
+   const CleanResults = await CleanHome()
+   console.log(CleanResults);
+
+   const CarResults = await CarWash()
+   console.log(CarResults);
+
+   }
+   catch(error){
+    console.error("Error Please Check code Again")
+   }
+}
+
+DoChores();
+
+
+async function RestOperators(){
+  try{
+    const RestAction1 = await res();
+   
+  console.log(RestAction1);
+
+  const RestAction2 = await res();
+   
+  console.log(RestAction2);
+
+  const RestAction3 = await res();
+   
+  console.log(RestAction3);
+  }
+  
+  catch(error){
+    console.error("Error is Formed");
+  }
+
+}
+
+TaskOperators();
